8ad09fd37da1226178d5e00f5f1ffd4ec3255ba8
henrychoi7 <wschd770@gmail.com>
2017-11-21 01:56:34 +0900

    [v0.3] - 모바일 앱 인터페이스, 서비스, 모델 구성

---
+++ Client/app/src/main/java/com/example/wschd/client/interfaces/RetrofitInterface.kt
@@ -0,0 +1,21 @@
+package com.example.wschd.client.interfaces
+
+import com.example.wschd.client.model.Model
+import io.reactivex.Observable
+import okhttp3.RequestBody
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface RetrofitInterface {
+
+    @POST("login")
+    fun postLogin(@Body paras: RequestBody): Observable<Model.DefaultResponse>
+
+    @POST("register")
+    fun postRegister(@Body params: RequestBody): Observable<Model.DefaultResponse>
+
+    @GET("my_point")
+    fun getMyPoint(): Observable<Model.SingleIntResponse>
+}
---
+++ Client/app/src/main/java/com/example/wschd/client/services/SmsBroadcastReceiver.kt
@@ -0,0 +1,43 @@
+package com.example.wschd.client.services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import com.example.wschd.client.services.RegisterActivity
+
+class SmsBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == "android.provider.Telephony.SMS_RECEIVED") {
+            val bundle: Bundle? = intent.extras  // Bundle객체에 문자를 받아온다
+            if (bundle != null) {
+                val pduObjects = bundle.get("pdus") as Array<*>
+                for (i in pduObjects.indices) {
+                    val currentSms = getIncomingMessage(pduObjects[i] as ByteArray, bundle)
+                    //val senderNo = currentSms.displayOriginatingAddress
+                    val message = currentSms.displayMessageBody
+                    if (message.contains("LottoVillage")) {
+                        val certifiedNumber = message.substring(message.indexOf("[") + 1, message.indexOf("]"))
+                        intent.putExtra("certified_number", certifiedNumber)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                        intent.setClass(context, RegisterActivity::class.java)
+                        context.startActivity(intent)
+                    }
+                }
+                this.abortBroadcast()
+            }
+        }
+    }
+
+    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val format = bundle.getString("format")
+            return SmsMessage.createFromPdu(aObject as ByteArray, format)
+        } else {
+            return SmsMessage.createFromPdu(aObject as ByteArray)
+        }
+    }
+}
