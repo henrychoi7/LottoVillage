e262efa5a648c4da305488ef4bcd08a2ce55ee6d
030ii <lovelygeee2@gmail.com>
2017-12-05 13:22:21 +0900

    [v.0.9] - 웹 클라이언트 메인페이지 데이터 표시 추가

--- Client_web/assets/js/pages/base_pages_dashboard_v2.js
+++ Client_web/assets/js/pages/base_pages_dashboard_v2.js
@@ -4,7 +4,38 @@
  *  Description: Custom JS code used in Dashboard v2 Page
  */
 
+// var url = "http://13.124.207.144/";
+var url = "http://203.249.127.32:65004/";
+
+// var myJson = {"isSuccess":true,"results":{"week_lv_date":["12/05","12/04","12/03","12/02","12/01","11/30","11/29"],"week_lv_one_cnt":[3,1,2,3,1,1,1],"week_lv_six_cnt":[1,2,3,1,1,1,1],"week_lv_twelve_cnt":[1,1,1,1,1,1,1]}};
+// console.log(myJson.results.week_lv_date);
+var weekDate = ['12/03', '12/04', '12/15', '12/16', '12/17', '12/17', '12/18'];
+var week1 = [600, 350, 1100, 420, 750, 1050, 670];
+var week6 = [490, 430, 560, 790, 1200, 950, 1500];
+var week12 = [520, 410, 120, 230, 420, 800, 1000];
+
+
 var BasePagesDashboardv2 = function() {
+    var getWeekChart = function() {
+        $.ajax({
+            type: "GET",
+            url: url + 'week_lv',
+            dataType: "json",
+            success: function(resData) {
+                if (resData.isSuccess == true){
+                    weekDate = resData.results.week_lv_date;
+                    week1 = resData.results.week_lv_one_cnt;
+                    week6 = resData.results.week_lv_six_cnt;
+                    week12 = resData.results.week_lv_twelve_cnt;
+                }
+            },
+            error: function(request,status,error){
+                alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+            }
+        });
+    };
+
+
     // Chart.js Chart, for more examples you can check out http://www.chartjs.org/docs
     var initDashv2ChartJS = function(){
         // Get Chart Container
@@ -13,27 +44,38 @@ var BasePagesDashboardv2 = function() {
 
         // Earnings Chart Data
         var $dashChartEarningsData = {
-            labels: ['12/03', '12/04', '12/15', '12/16', '12/17', '12/17', '12/18'],
+            // labels: ['12/03', '12/04', '12/15', '12/16', '12/17', '12/17', '12/18'],
+            labels: weekDate,
             datasets: [
                 {
-                    label: 'Last Week',
+                    label: '1시간',
                     fillColor: 'rgba(68, 180, 166, .07)',
                     strokeColor: 'rgba(68, 180, 166, .25)',
                     pointColor: 'rgba(68, 180, 166, .25)',
                     pointStrokeColor: '#fff',
                     pointHighlightFill: '#fff',
                     pointHighlightStroke: 'rgba(68, 180, 166, 1)',
-                    data: [600, 350, 1100, 420, 750, 1050, 670]
+                    data: week1
                 },
                 {
-                    label: 'This Week',
+                    label: '6시간',
                     fillColor: 'rgba(68, 180, 166, .25)',
                     strokeColor: 'rgba(68, 180, 166, .55)',
                     pointColor: 'rgba(68, 180, 166, .55)',
                     pointStrokeColor: '#fff',
                     pointHighlightFill: '#fff',
                     pointHighlightStroke: 'rgba(68, 180, 166, 1)',
-                    data: [490, 430, 560, 790, 1200, 950, 1500]
+                    data: week6
+                },
+                {
+                    label: '12시간',
+                    fillColor: 'rgba(68, 180, 166, .25)',
+                    strokeColor: 'rgba(68, 180, 166, .75)',
+                    pointColor: 'rgba(68, 180, 166, .75)',
+                    pointStrokeColor: '#fff',
+                    pointHighlightFill: '#fff',
+                    pointHighlightStroke: 'rgba(68, 180, 166, 1)',
+                    data: week12
                 }
             ]
         };
@@ -92,6 +134,7 @@ var BasePagesDashboardv2 = function() {
         init: function () {
             // Init ChartJS charts
             initDashv2ChartJS();
+            // getWeekChart();
         }
     };
 }();
