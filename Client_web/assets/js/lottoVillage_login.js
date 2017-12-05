4135175e2caa2a8b2791651b803b8e6f4b553d03
030ii <lovelygeee2@gmail.com>
2017-12-05 10:14:01 +0900

    [v.0.9] - 웹 클라이언트 / 서버 통신, 관리자 로그인, 회원 조회, 회원 정지, 상품/포인트/복권 내역 보기 구현

---
+++ Client_web/assets/js/lottoVillage_login.js
@@ -0,0 +1,46 @@
+$(window).ready(function() {
+    login.init();
+});
+
+// var url = "http://13.124.207.144/";
+var url = "http://203.249.127.32:65004/";
+
+
+var login = {
+    init: function() {
+        this.initEvent();
+    },
+
+    initEvent: function() {
+        var _this = this;
+
+        $(document).on('click touchend','.login-btn', this.postLogin);
+    },
+
+    postLogin: function() {
+        var $form = $('form[name=admin-login]');
+        var $name = $form.find('input[name=login-username]');
+        var $password = $form.find('input[name=login-password]');
+        console.log('asdfdsdf');
+
+        $.ajax({
+            type: "POST",
+            url: url + 'login_web',
+            dataType: "json",
+            data: {
+                phone_number : $name.val(),
+                password : $password.val()
+            },
+            success: function(resData) {
+                if (resData.isSuccess == true){
+                    window.location.href = '/Client_web/lottoVillage_main.html';
+                } else {
+                    alert('로그인에 실패하셨습니다. 다시 로그인해주세요!');
+                }
+            },
+            error: function(request,status,error){
+                alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+            }
+        });
+    }
+};
